4f2d43ba64247fd9b2ac129a53af227c90b8eee4
simpigo <simpigo@sina.com>
2021-01-08 00:52:20 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,8 +4,25 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title', 'Weibo App') - Laravel 学习</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-  @yield('content')
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a href="/" class="navbar-brand">Weibo App</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item">
+          <a href="/help" class="nav-link">帮助</a>
+        </li>
+        <li class="navbar-nav">
+          <a href="#" class="nav-link">登录</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    @yield('content')
+  </div>
 </body>
 </html>
