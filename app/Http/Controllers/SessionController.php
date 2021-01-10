bf72d8908a89925d57f8f3e92184dd5ddf200fa3
simpigo <simpigo@sina.com>
2021-01-10 23:46:49 +0000

    访问策略

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -8,6 +8,11 @@ use Auth;
 
 class SessionController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', ['only' => ['create']]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -19,11 +24,10 @@ class SessionController extends Controller
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
-        $email = $request->email;
-        $password = $request->password;
         if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', '欢迎回来~');
-            return redirect()->route('users.show', [Auth::user()]);
+            $fallback = route('users.show', Auth::user());
+            return redirect()->intended($fallback);
         } else {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,8 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 
+use Auth;
+
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show', 'create', 'store']]);
+        $this->middleware('guest', ['only' => ['create']]);
+    }
+
     public function create()
     {
         return view('users.create');
@@ -36,11 +44,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
